4077ee365837e11db99c085852ad92580a09c56b
narumi <sychoi2008@sookmyung.ac.kr>
2024-10-19 00:21:08 +0900

    first commit

---
+++ com/ex/TestSecurityBasic/TestSecurityBasicApplication.java
@@ -0,0 +1,13 @@
+package com.ex.TestSecurityBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSecurityBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSecurityBasicApplication.class, args);
+	}
+
+}
